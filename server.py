321beb0bff0435759431cc09c7c2b4ea453e73bd
ianleonardo <ianleonardo@yahoo.com>
2025-04-13 17:29:57 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,30 @@
+"""
+This program to start server for Emotion Detection
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 ## Initiate Flask app
 app = Flask(__name__)
 
-# Define a route for the root URL ("/")
 @app.route("/")
 def home():
+    """Define a route for the root URL ("/")
+    Returns:
+        The return value of the function.
+    """
     return render_template('index.html')
 
-## Define a rourte for URL ("/emotionDetector")
 @app.route("/emotionDetector")
-def emotionDetector():
-    textToAnalyze = request.args.get("textToAnalyze")
-    result = emotion_detector(textToAnalyze)
-    
+def call_emotion():
+    """Define a rourte for URL ("/emotionDetector")
+    Returns:
+        The emotion list of given text
+    """
+    text = request.args.get("textToAnalyze")
+    result = emotion_detector(text)
+
     if result['dominant_emotion'] is None:
         return 'Invalid text! Please try again!'
-    else:
-        return result
+
+    return result
